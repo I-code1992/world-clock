7606f509ce2d7658e18d9262b26bc5e7c390304f
I-code1992 <138527013+I-code1992@users.noreply.github.com>
2025-05-13 16:58:49 +0200

    added city moment js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,28 @@
+//losangeles
 let losangelesDateElement =document.querySelector("#los-angeles .date");
 losangelesDateElement.innerHTML= moment().format("MMM Do YYYY");
 let losangelesTimeElement =document.querySelector("#los-angeles .time");
 losangelesTime = moment().tz("America/Los_Angeles");
-losangelesTimeElement.innerHTML= losangelesTime.format("h:mm:ss A");
+losangelesTimeElement.innerHTML= losangelesTime.format("h:mm:ss [<small>]A[</small>]");
+
+//shanghai
+let shanghaiDateElement =document.querySelector("#Shanghai .date");
+shanghaiDateElement.innerHTML= moment().format("MMM Do YYYY");
+let shanghaiTimeElement =document.querySelector("#Shanghai .time");
+shanghaiTime = moment().tz("Asia/Shanghai");
+shanghaiTimeElement.innerHTML= shanghaiTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+//Kuala lumpur
+let kualalumpurDateElement =document.querySelector("#kuala-lumpur .date");
+kualalumpurDateElement.innerHTML= moment().format("MMM Do YYYY");
+let kualalumpurTimeElement =document.querySelector("#kuala-lumpur .time");
+kualalumpurTime = moment().tz("Asia/Kuala_Lumpur");
+kualalumpurTimeElement.innerHTML= kualalumpurTime.format("h:mm:ss [<small>]A[</small>]");
+
+//Lisbon
+let lisbonDateElement =document.querySelector("#lisbon .date");
+lisbonDateElement.innerHTML= moment().format("MMM Do YYYY");
+let lisbonTimeElement =document.querySelector("#lisbon .time");
+lisbonTime = moment().tz("Europe/Lisbon");
+lisbonTimeElement.innerHTML= lisbonTime.format("h:mm:ss [<small>]A[</small>]");
